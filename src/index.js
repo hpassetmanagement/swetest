2d011f284a85d962fba468a303d1dbfef5b3edb7
Ridho <ridhoadhadigani@gmail.com>
2024-04-30 15:40:16 +0700

    feat: add comment to clarify index.js purpose as application entry point

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// This is the entry point of the application.
 ReactDOM.render(<App />, document.querySelector("#root"));
